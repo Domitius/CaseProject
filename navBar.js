44b6fe678ad4000495be5d5d15060d69a182c4ff
Gavin Burris <domitius.rc@gmail.com>
2018-09-08 13:54:25 -0700

    Updating nav bar links

--- navBar.js
+++ navBar.js
@@ -1,6 +1,6 @@
 document.write('<div class="topnav">');
 document.write('<img src="./logo.png" alt="logo">');
-document.write('<a href="someFileName.html">Gavin\'s Page</a>');
+document.write('<a href="burrisMovies.html">Gavin\'s Page</a>');
 document.write('<a href="someFileName.html">Phillip\'s Page</a>');
 document.write('<a href="./index.html" class="active">Home</a>');
 document.write('</div>');
