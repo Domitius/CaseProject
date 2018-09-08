2aab23bac46ec0af98270e5a034fa5790f31ba14
Gavin Burris <domitius.rc@gmail.com>
2018-09-08 13:57:19 -0700

    Removing active class

--- navBar.js
+++ navBar.js
@@ -2,5 +2,5 @@ document.write('<div class="topnav">');
 document.write('<img src="./logo.png" alt="logo">');
 document.write('<a href="burrisMovies.html">Gavin\'s Page</a>');
 document.write('<a href="someFileName.html">Phillip\'s Page</a>');
-document.write('<a href="./index.html" class="active">Home</a>');
+document.write('<a href="./index.html">Home</a>');
 document.write('</div>');
