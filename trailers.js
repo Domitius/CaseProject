67e741a82b34c23e0f58ba52cd9cca76a493d169
Philip R <rogersph@otc.edu>
2018-09-08 23:18:29 -0500

    Fixed navBar.js and added trailer function to philipGames.html
    Redirected Philip's navbar link to his html page. Updated game reviews with existing trailer functionality.

--- trailers.js
+++ trailers.js
@@ -3,6 +3,11 @@ var wolfofwallstreet = '<iframe width="100%" height="400px" src="https://www.you
 var spiderman = '<iframe width="100%" height="400px" src="https://www.youtube.com/embed/n9DwoQ7HWvI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
 var prometheus = '<iframe width="100%" height="400px" src="https://www.youtube.com/embed/34cEo0VhfGE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
 var exmachina = '<iframe width="100%" height="400px" src="https://www.youtube.com/embed/EoQuVnKhxaM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+var celeste = '<iframe width="100%" height="400px" src="https://www.youtube.com/embed/iofYDsA2yqg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+var frostpunk = '<iframe width="100%" height="400px" src="https://www.youtube.com/embed/qqEpSOFDXGA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+var octopath = '<iframe width="100%" height="400px" src="https://www.youtube.com/embed/Fmi8KrntszI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+var battleforazeroth = '<iframe width="100%" height="400px" src="https://www.youtube.com/embed/jSJr3dXZfcg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+var godofwar = '<iframe width="100%" height="400" src="https://www.youtube.com/embed/K0u_kAWLJOA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
 
 function trailer(embedlink, movietitle) {
   var div = document.getElementById(movietitle);
