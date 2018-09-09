bef67c6ecbd73a0efcf781f75b272ac864fbb86d
Gavin Burris <domitius.rc@gmail.com>
2018-09-09 13:03:33 -0700

    Delete navBar.js

--- navBar.js
+++
@@ -1,6 +0,0 @@
-document.write('<div class="topnav">');
-document.write('<img src="./logo.png" alt="logo">');
-document.write('<a href="burrisMovies.html">Gavin\'s Page</a>');
-document.write('<a href="philipGames.html">Phillip\'s Page</a>');
-document.write('<a href="./index.html">Home</a>');
-document.write('</div>');
