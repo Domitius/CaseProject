b8158059489a35e561a4b8e69e3ec8863cba82bf
Gavin Burris <domitius.rc@gmail.com>
2018-09-09 15:09:39 -0500

    Uploading project with new folder structure.
    Finished integration for phase 1.

---
+++ CaseProject-master/js/navBar.js
@@ -0,0 +1,6 @@
+document.write('<div class="topnav">');
+document.write('<img src="images/logo.png" alt="logo">');
+document.write('<a href="burrisMovies.html">Gavin\'s Page</a>');
+document.write('<a href="philipGames.html">Phillip\'s Page</a>');
+document.write('<a href="./index.html">Home</a>');
+document.write('</div>');
